c8f37197b1872e766157a379122ade1bfec10d8c
Mira-M <mira.mollar@gmail.com>
2015-08-07 21:16:40 -0400

    Added markup for blog title overflow issues.

--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -6,14 +6,16 @@
  */
 ?>
 
+
 <!-- PORTFOLIO IMAGE 1 -->
 <div class="col-sm-6 col-md-4 item">
     <div class="grid grid-blog">
 		<figure>
-			<?php the_post_thumbnail( 'full', array( 'class' => 'img-responsive') ); ?>
+			<?php the_post_thumbnail( 'full', array( 'class' => 'img-responsive') ); ?>	
 		<figcaption>
-			<a id="blogcap" href="<?php the_permalink(); ?>" <h5><?php the_title(); ?></h5></a>
+			<a href="<?php the_permalink(); ?>" <h5><?php the_title(); ?></h5></a>
 		</figcaption><!-- /figcaption -->
+		
 		</figure><!-- /figure -->
 	</div><!-- /grid-mask -->
 </div><!-- /col -->

88b3729db273156972d21585a23c71479599823b
Mira-M <mira.mollar@gmail.com>
2015-08-09 12:49:39 -0400

    Refactored the home page blog title to reduce the size of titles over 45 characters.

--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -6,6 +6,12 @@
  */
 ?>
 
+<?php
+	
+	$home_blog_title = the_title_attribute('echo=0');
+	$blog_loop_value = strlen($home_blog_title);
+?>
+
 
 <!-- PORTFOLIO IMAGE 1 -->
 <div class="col-sm-6 col-md-4 item">
@@ -13,7 +19,16 @@
 		<figure>
 			<?php the_post_thumbnail( 'full', array( 'class' => 'img-responsive') ); ?>	
 		<figcaption>
-			<a href="<?php the_permalink(); ?>" <h5><?php the_title(); ?></h5></a>
+			<?php
+				if ($blog_loop_value > 45) {
+					echo '<a href="';
+					echo the_permalink() . '" <h5 id ="blog_title_overflow">' . get_the_title() . '</h5></a>';
+				} else {
+					echo '<a href="';
+					echo the_permalink() . '" <h5>' . get_the_title() . '</h5></a>';
+				}
+			?>
+		
 		</figcaption><!-- /figcaption -->
 		
 		</figure><!-- /figure -->

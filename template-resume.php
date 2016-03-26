64f419ff7595819cb862b51a03b94c6d9a8ab340
Mira-M <mira.mollar@gmail.com>
2016-03-25 21:39:31 -0400

    Updated Social Media Options

--- template-resume.php
+++ template-resume.php
@@ -78,13 +78,12 @@
               <div class="col-xs-9">
                 <div class="profession">Software Engineer I</div>
                 <div class="description"><ul><li>Assists Project Managers and teammates in preparing and facilitating customer demonstrations and status meetings. </li>
-                	<li>Collaborates with developers, SCRUM master, Project Managers, and clients to determine the best approach to implement software requirements.   
- </li>
-                  <li>Developed REST-ful style Services layer that interacts with Keycloak, an external authentication server that manages the users for the application.  </li>
-                  <li>Implemented the Administration Panel using Angular JS, for managing Clinician and Administrator roles, and updating legal agreements. 
-</li>
-				<li>Collaborated with team member to implement a document repository using Amazon S3 services with an interface in Angular JS, for users to upload and manage files and other resources.</li>
-                <li>Works side-by-side with clients during testing, providing live technical support, through identifying bugs and offering training where needed. </li>
+                	<li>Collaborates with developers, SCRUM master, Project Managers, and clients to determine the best approach to implement software requirements.</li>
+					<li>Developed REST-ful style Services layer that interacts with Keycloak, an external authentication server that manages the users for the 
+						application.</li>
+					<li>Implemented the Administration Panel using Angular JS, for managing Clinician and Administrator roles, and updating legal agreements.</li>
+					<li>Collaborated with team member to implement a document repository using Amazon S3 services with an interface in Angular JS, for users to upload and 							manage files and other resources.</li>
+					<li>Works side-by-side with clients during testing, providing live technical support, through identifying bugs and offering training where needed. </li>
                 </ul></div>
               </div>
             </div>
@@ -128,14 +127,15 @@
             	<div class="col-xs-9">
                 <div class="profession">General Manager</div>
                 <div class="year">2010 - 2013</div>
-                <div class="description"><ul><li>Trained and led store team to work together cohesively to accomplish store-wide goals. </li>
-                  <li>Developed processes that delivered increased customer satisfaction results.</li>
-                    <li>Exceeded operational standards through controlling store revenue and costs.</li>
-                    <li>Played a pivotal role in the success of a region-wide initiative to retain female employees and customers, through co-founding a 							leadership development program for female leaders.</li>
-                </ul></div>
+                <div class="description">
+	                <ul><li>Trained and led store team to work together cohesively to accomplish store-wide goals. </li>
+                  		<li>Developed processes that delivered increased customer satisfaction results.</li>
+				  		<li>Exceeded operational standards through controlling store revenue and costs.</li>
+				  		<li>Played a pivotal role in the success of a region-wide initiative to retain female employees and customers, through co-founding a 											leadership development program for female leaders.</li>
+                	</ul></div>
             		<div class="profession">Geek Squad Manager - Tier 1/Tier 2 Customer Support</div>
             		<div class="year">2004 - 2010</div>
-            		<div class="description"><ul><li>Performed computer hardware installations on Windows and Mac devices, such as memory, hard drives, optical 					drives and power supply units.</li>
+            		<div class="description"><ul><li>Performed computer hardware installations on Windows and Mac devices, such as memory, hard drives, optical 								drives and power supply units.</li>
 	            	<li>Planned, designed and installed user computer systems based on user specifications.</li>
 	            	<li>Collaborated with team members to develop a new process that helped reduce customer completion time by 20%.</li>
 	            	<li>Diagnosed software issues, performed system updates, and installed new software.</li>
@@ -157,17 +157,17 @@
               <div class="title only pull-right">mira@miramollar.com</div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><a href="https://twitter.com/coderbug83"><span class="fa fa-twitter fa-fw"></span></a></div>
+              <div class="icon pull-left text-center"><a href="https://twitter.com/coderbug83" target="_blank"><span class="fa fa-twitter fa-fw"></span></a></div>
               <div class="title pull-right">Twitter</div>
               <div class="description pull-right">https://twitter.com/coderbug83</div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><a href="https://www.facebook.com/coderbug83"><span class="fa fa-facebook fa-fw"></span></a></div>
+              <div class="icon pull-left text-center"><a href="https://www.facebook.com/coderbug83" target="_blank"><span class="fa fa-facebook fa-fw"></span></a></div>
               <div class="title pull-right">Facebook</div>
               <div class="description pull-right">https://www.facebook.com/coderbug83</div>
             </div>
             <div class="contact-item">
-              <div class="icon pull-left text-center"><a href="https://github.com/Mira-M"><span class="fa fa-github-alt fa-fw"></span></a></div>
+              <div class="icon pull-left text-center"><a href="https://github.com/Mira-M" target="_blank"><span class="fa fa-github-alt fa-fw"></span></a></div>
               <div class="title pull-right">GitHub</div>
               <div class="description pull-right">https://github.com/Mira-M</div>
             </div>
